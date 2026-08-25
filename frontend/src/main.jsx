a6f21304a65f86b07812a4cd7bc4bdd30d6da584
yashdewangan850-hub <yashdewangan850@gmail.com>
2026-08-25 14:33:25 +0530

    fix

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -6,8 +6,14 @@ import "./styles.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    
+<BrowserRouter
+  future={{
+    v7_startTransition: true,
+    v7_relativeSplatPath: true,
+  }}
+>
+  <App />
+</BrowserRouter>
   </React.StrictMode>
 );
